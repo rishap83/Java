d2bd0aa09947e976055f0cc96d09542dcdcda1ae
Rishap <rishap_k@trimble.com>
2021-03-13 23:18:41 +0530

    added truncate code

--- JavaDemo.java
+++ JavaDemo.java
@@ -1,6 +1,14 @@
 public class JavaDemo{
 	public static void main(String[] args) {
         System.out.println("hello");
+
+	float f=123.456f;
+        Float f1=new Float(f);
+        System.out.println(f1);
+        int i= (int) f;
+        System.out.println("my number="+i);
+
+        System.out.println("Float= "+f1.intValue());
         }
 
 }
